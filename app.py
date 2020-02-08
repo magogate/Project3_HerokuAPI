463b3805f5890dc3db3cf5a82e062c27aeab8182
magogate <mandargogate@outlook.com>
2020-02-07 19:36:49 -0500

    Added code for city population accidents of GA

--- app.py
+++ app.py
@@ -22,18 +22,25 @@ def index():
                 <table>
                 
                     <tr>
-                        <td> To get city json - use </td>
-                        <td> /city </td>
+                        <td> To get all USA Cities json for Accidents & Population - use </td>
+                        <td> /allcities </td>
                     </tr>                   
-                
+                    <tr>
+                        <td> To get only GA Cities json for Accidents & Population - use </td>
+                        <td> /gacities </td>
+                    </tr>              
                 <table>
                 <html>
             """
     return htmlTag
 
-@app.route("/city")
-def cityPopulation():
-    return db.getCityPopulation()
+@app.route("/allcities")
+def all_city_Population():
+    return db.getUSCityPopulation()
+
+@app.route("/gacities")
+def ga_city_Population():
+    return db.getGACityPopulation()
 
 if __name__ == "__main__":
     # app.run(debug=True)
